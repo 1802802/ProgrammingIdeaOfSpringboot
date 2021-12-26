5a505349e45ee8ebb6ea73382573c487aba43cda
1802802 <35828295+1802802@users.noreply.github.com>
2021-12-26 21:58:34 +0800

    项目初始化

---
+++ src/main/java/com/programming/idea/of/springboot/programmingideaofspringboot/ProgrammingIdeaOfSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.programming.idea.of.springboot.programmingideaofspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingIdeaOfSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammingIdeaOfSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/programming/idea/of/springboot/programmingideaofspringboot/ProgrammingIdeaOfSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programming.idea.of.springboot.programmingideaofspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingIdeaOfSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
